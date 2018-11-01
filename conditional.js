cf289ca322bf5a5d93c190f15930710bf61174d1
Galvanize Web Development <Galvanize@Isaacs-MacBook-Pro.local>
2018-11-01 11:41:28 -0600

    added index.html

---
+++ conditional.js
@@ -0,0 +1,10 @@
+var num = 13
+if (num < 13) {
+    console.log('foolish mortal')
+} else if (num > 13){
+    console.log('you seem to be lacking in the luck department')
+} else {
+    console.log('HELLO MASTER')
+}
+    
+
